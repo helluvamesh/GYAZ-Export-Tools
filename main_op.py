d129f10f5fbbb6682e290c751b8950b46c38fc7e
helluvamesh <helluvamesh@gmail.com>
2019-05-07 01:07:48 +0200

    Add files via upload

--- main_op.py
+++ main_op.py
@@ -307,7 +307,10 @@ class Op_GYAZ_Export_Export (bpy.types.Operator):
                 
             # make all collectios visible
             for collection in bpy.context.scene.collection.children:
-                collection.hide_viewport = False
+                collection.hide_viewport, collection.hide_select = False, False
+                for obj in collection.objects:
+                    obj.hide_viewport, obj.hide_select = False, False
+                    obj.hide_set (False)
                 
             
             length = len (scene.gyaz_export.extra_bones)    
